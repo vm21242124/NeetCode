125918a23d7b818ef6f43db66a65be8c914cda17
vm21242124 <vm21242124@gmail.com>
2023-07-19 20:44:11 +0530

    adding dependency

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import AdminPanel from "./Pages/AdminPanel/AdminPanel";
 import HomePage from "./Pages/HomePage/HomePage";
 import ProblemPage from "./Pages/ProblemPage/ProblemPage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
-
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.withCredentials = true;
 function App() {
   return (
     <>
-    {/* <HomePage/> */}
+      {/* <HomePage/> */}
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/problem" element={<ProblemPage />} />
           <Route exact path="/admin" element={<AdminPanel />} />
-          <Route exact path="/login" element={<LoginPage/>}/>
+          <Route exact path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </>
