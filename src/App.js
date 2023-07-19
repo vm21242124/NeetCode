ba59711663935b62ca667b550a0543370ac1d1d3
vm21242124 <vm21242124@gmail.com>
2023-07-19 18:46:41 +0530

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminPanel from "./Pages/AdminPanel/AdminPanel";
 import HomePage from "./Pages/HomePage/HomePage";
 import ProblemPage from "./Pages/ProblemPage/ProblemPage";
-
+import LoginPage from "./Pages/LoginPage/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      {/* <AdminPanel/> */}
-      {/* <HomePage/> */}
-      <ProblemPage/>
-       
-    </div>
+    <>
+    {/* <HomePage/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/problem" element={<ProblemPage />} />
+          <Route exact path="/admin" element={<AdminPanel />} />
+          <Route exact path="/login" element={<LoginPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
