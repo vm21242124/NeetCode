fdf9edd935227de0d4d868f69f7af89f6bccb288
vm21242124 <vm21242124@gmail.com>
2023-07-19 13:35:46 +0530

    tempdata to render

---
+++ src/tempdata.js
@@ -0,0 +1,119 @@
+export const problemList = [
+  {
+    id: 1,
+    title: "Reverse a String",
+    difficulty: "Easy",
+    category: "Strings",
+  },
+  {
+    id: 2,
+    title: "Binary Search",
+    difficulty: "Medium",
+    category: "Searching",
+  },
+  {
+    id: 3,
+    title: "Merge Sort",
+    difficulty: "Hard",
+    category: "Sorting",
+  },
+  {
+    id: 4,
+    title: "Linked List Cycle Detection",
+    difficulty: "Medium",
+    category: "Linked Lists",
+  },
+  {
+    id: 5,
+    title: "Reverse a String",
+    difficulty: "Easy",
+    category: "Strings",
+  },
+  {
+    id: 6,
+    title: "Binary Search",
+    difficulty: "Medium",
+    category: "Searching",
+  },
+  {
+    id: 7,
+    title: "Merge Sort",
+    difficulty: "Hard",
+    category: "Sorting",
+  },
+  {
+    id: 8,
+    title: "Linked List Cycle Detection",
+    difficulty: "Medium",
+    category: "Linked Lists",
+  },
+  {
+    id: 9,
+    title: "Reverse a String",
+    difficulty: "Easy",
+    category: "Strings",
+  },
+  {
+    id: 10,
+    title: "Binary Search",
+    difficulty: "Medium",
+    category: "Searching",
+  },
+  {
+    id: 11,
+    title: "Merge Sort",
+    difficulty: "Hard",
+    category: "Sorting",
+  },
+  {
+    id: 12,
+    title: "Linked List Cycle Detection",
+    difficulty: "Medium",
+    category: "Linked Lists",
+  },
+  // Add more problem objects here
+];
+export const problem = {
+  title: "1. Two Sum",
+  discription:
+    "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target \n\n You may assume that each input would have \n\n exactly one solution, and you may not use the same element twice.\n\n You can return the answer in any order.",
+    difficulty:"easy",
+    expectedtc:"Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?",
+  examples: [
+    {
+      id: 1,
+      input: "nums = [2,7,11,15], target = 9",
+      output: "[0,1]",
+      Explanation: "Because nums[0] + nums[1] == 9, we return [0, 1].",
+    },
+    {
+      id: 2,
+      input: "nums = [2,7,11,15], target = 9",
+      output: "[0,1]",
+      Explanation: "Because nums[0] + nums[1] == 9, we return [0, 1].",
+    },
+    {
+      id: 3,
+      input: "nums = [2,7,11,15], target = 9",
+      output: "[0,1]",
+      Explanation: "Because nums[0] + nums[1] == 9, we return [0, 1].",
+    },
+  ],
+  constraint:[
+    {
+      id:1,
+      title:"2 <= nums.length <= 104"
+    },
+    {
+      id:2,
+      title:"2 <= nums.length <= 104"
+    },
+    {
+      id:3,
+      title:"2 <= nums.length <= 104"
+    }
+  ]
+  
+};
+
+export const javasnipet="class NeetCode {\n   public static void main(String[] args) {\n      System.out.println('hello world'));\n }\n}"
