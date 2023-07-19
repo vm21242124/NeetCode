ed5005e95cda61f4dcb0c3947a6a0561c4cafd3c
vm21242124 <vm21242124@gmail.com>
2023-07-19 13:35:35 +0530

    initializing main files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import AdminPanel from "./Pages/AdminPanel/AdminPanel";
+import HomePage from "./Pages/HomePage/HomePage";
+import ProblemPage from "./Pages/ProblemPage/ProblemPage";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AdminPanel/> */}
+      {/* <HomePage/> */}
+      <ProblemPage/>
+       
+    </div>
+  );
+}
+
+export default App;
